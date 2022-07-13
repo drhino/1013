19bc3596516a9847f521ac83813bd9ef074c6d14
Gerd <gerdachternaam@gmail.com>
2022-07-13 05:03:55 +0200

    Update disableService.sh

--- sh/disableService.sh
+++ sh/disableService.sh
@@ -23,37 +23,57 @@
 #
 # STDOUT: "disableService(): com.apple.xxx: OK"
 # STDERR: "disableService(): com.apple.xxx: FAILURE"
+#
 # STDERR: "/System/Library/LaunchDaemons/com.apple.xxx.plist: Could not find specified service"
 #
 #
+CURRENT_DIR="$(cd "$(dirname "${0}")" && pwd)"
+
+#
+# Unloads the service immediately.
+#
+function unloadService() {
+
+	if [[ ! -z $(sudo launchctl list | grep "${1}$") ]]
+	then
+		if [[ -z "${2}" ]]
+		then
+			if [[ -z "${SERVICE_MAP}" ]]
+			then
+				SERVICE_MAP="$(cat "${CURRENT_DIR}/disableService.sh.map")"
+			fi
+
+			# Finds the absolute path to the .plist.
+			plist=$(echo "${SERVICE_MAP}" | grep "${1}" | awk '{print $2}')
+
+			if [[ -z "${plist}" ]]
+			then
+				echo "DEBUG: disableService(): .plist not defined for: ${1}" 1>&2
+			else
+				#
+				# Prints to STDERR on failure with message:
+				# "/System/Library/LaunchDaemons/com.apple.xxx.plist: Could not find specified service"
+				#
+				# Prints nothing on success.
+				#
+				sudo launchctl unload -w "${plist}"
+
+				unloadService "${1}" --check
+			fi
+		else
+			echo "disableService(): ${1}: UNLOAD: FAILURE" 1>&2
+		fi
+	elif [[ ! -z "${2}" ]]
+	then
+		echo "disableService(): ${1}: UNLOAD: OK"
+	fi
+}
+
+#
+# Disables the service after a reboot.
+# 
 function disableService() {
 
-	CURRENT_DIR="$(cd "$(dirname "${0}")" && pwd)"
-
-	# Unloads the service immediately.
-	if [[ ! -z $(launchctl list | grep "${1}$") ]]
-	then
-		# Finds the absolute path to the .plist.
-		plist=$(cat "${CURRENT_DIR}/disableService.sh.map" | grep "${1}" | awk '{print $2}')
-
-		if [[ ! -z "${plist}" ]]
-		then
-			#
-			# Prints to STDERR on failure with message:
-			# "/System/Library/LaunchDaemons/com.apple.xxx.plist: Could not find specified service"
-			#
-			# Prints nothing on success.
-			#
-			sudo launchctl unload -w "${plist}"
-		else
-			echo "DEBUG: disableService(): .plist not defined for: ${1}" 1>&2
-		fi
-	#elif [[ ! -z "${2}" ]]
-	#then
-	#	echo "disableService(): ${1}: UNLOADED: OK"
-	fi
-
-	# Disables the service after reboot.
 	if [[ -z $(sudo launchctl print-disabled system | grep "\"${1}\" => true") ]]
 	then
 		if [[ -z "${2}" ]]
@@ -71,3 +91,15 @@ function disableService() {
 }
 
 disableService "${1}" "${2}"
+
+if [[ -z "${2}" ]]
+then
+	unloadService "${1}"
+else
+	if [[ -z $(sudo launchctl list | grep "${1}$") ]]
+	then
+		echo "disableService(): ${1}: STOPPED"
+	else
+		echo "disableService(): ${1}: RUNNING" 1>&2
+	fi
+fi
